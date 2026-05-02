eed9ad6d86e4da1af46cd2bf70472acb759be8af
ahmedanwarabdulaziz <208722087+ahmedanwarabdulaziz@users.noreply.github.com>
2026-05-02 14:53:33 +0300

    Update from local PC

--- next.config.js
+++ next.config.js
@@ -1,12 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: 'https',
                 hostname: 'pub-2814684ce1d74daeb80ca303dbb330e0.r2.dev',
                 pathname: '/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'firebasestorage.googleapis.com',
+                pathname: '/**',
+            },
+            {
+                protocol: 'https',
+                hostname: 'storage.googleapis.com',
+                pathname: '/**',
+            }
         ],
     },
 };
